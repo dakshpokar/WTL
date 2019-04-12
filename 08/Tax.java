de02eda1e40951d8a0d7d9ba0775d27f1d731f4c
Daksh Pokar <dakshpokar@gmail.com>
2019-04-12 02:11:40 -0700

    Assignment8

--- 08/Tax.java
+++ 08/Tax.java
@@ -1,9 +1,25 @@
-import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class Tax extends HttpServlet{
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-        response.setContentType("text/html");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    throws IOException, ServletException{
         PrintWriter out = response.getWriter();
-        out.println("Hi bro!");
+        Double amount = Double.parseDouble(request.getParameter("amount"));
+        out.println("<h1>Amount: " + amount + "</h1>");
+        Double tax = 0.0;
+        if(amount >= 250000){
+            if(amount <= 500000){
+                tax = amount * 0.1;
+            }
+            else{
+                tax = amount * 0.2;
+            }
+        }
+        out.println("<h1>Tax: " + tax + "</h1>");
     }
 }
