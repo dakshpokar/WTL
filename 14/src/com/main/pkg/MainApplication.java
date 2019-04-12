11d6f302149571c86490be69f664f515af8f65bc
Daksh Pokar <dakshpokar@gmail.com>
2019-04-12 06:39:16 -0700

    MySQL Connection

--- 14/src/com/main/pkg/MainApplication.java
+++ 14/src/com/main/pkg/MainApplication.java
@@ -1,17 +1,45 @@
 package com.main.pkg;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.context.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class MainApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("app.xml");
 		Student student = context.getBean("student", Student.class);
 		Student student1 = context.getBean("student", Student.class);
-		
-		System.out.println(student.toString());
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch(ClassNotFoundException x) {
+			System.out.println("Class Not Found!");
+		}
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/main", "dakshpokar", "password");
+			stmt = conn.createStatement();
 
-		System.out.println(student1.toString());
-		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ResultSet rs = null;
+		try {
+			rs = stmt.executeQuery("select * from Student");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(rs.next()) {
+			System.out.println(rs.getString(2));
+		}
 	}
 }
